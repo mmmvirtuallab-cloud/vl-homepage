8d25ff3596233af238957ac3c35908586c3ab9c4
Ismail <ismailisims1@gmail.com>
2025-11-04 12:25:29 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // FIX: Set the base to your repository name
+  // MUST HAVE TRAILING SLASH
   base: "/vl-homepage/", 
-  // Make sure this matches your GitHub repository name exactly!
 })
