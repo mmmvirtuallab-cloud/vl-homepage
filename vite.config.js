adac848a2fa3848a9d0211f32cd2bc435ca59a93
Ismail <ismailisims1@gmail.com>
2025-11-04 12:09:35 +0530

    git pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // FIX: Set the base to your repository name
+  base: "/vl-homepage/", 
+  // Make sure this matches your GitHub repository name exactly!
 })
